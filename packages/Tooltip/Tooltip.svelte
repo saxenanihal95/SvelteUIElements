3dcc4e72b865284654f9c3a4536a55a44befb953
Nihal Saxena <saxenanihal95@gmail.com>
2021-07-05 03:28:29 +0000

    Tooltip Component Implementation (#16)

---
+++ packages/Tooltip/Tooltip.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let title = "";
+</script>
+
+<div class="tooltip">
+  <slot />
+  <span class="tooltip__tooltiptext">{title}</span>
+</div>
+
+<style lang="scss" global>
+  @import "./Tooltip.scss";
+</style>
